b64ea3f5525be18961c538b9732cd66af6e63674
Anthony Medico <anthonymeds@gmail.com>
2025-11-09 16:07:20 -0500

    Added Spring Initializr backend

---
+++ backend/src/main/java/com/threadroseco/backend/ThreadRoseCoApiApplication.java
@@ -0,0 +1,13 @@
+package com.threadroseco.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadRoseCoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreadRoseCoApiApplication.class, args);
+	}
+
+}
