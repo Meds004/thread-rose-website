3e7d860a6bbbfce50dd1d04c9079159666a9d0a7
Anthony Medico <anthonymeds@gmail.com>
2025-12-01 12:51:08 -0500

    Initial commit - React front end basic structure setup

---
+++ frontend/src/pages/Cart.tsx
@@ -0,0 +1,9 @@
+export default function CartPage() {
+  return (
+    <>
+      <h2 className='text-2xl font-semibold'>
+      Cart
+      </h2>
+    </>
+  )
+}
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <h2 className='text-2xl font-semibold'>
+      Home Page
+    </h2>
+  )
+}
